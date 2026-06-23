6bff0068795a9a6c1c5a03b6b4addc096b07d2cf
naigel89 <naigel89@users.noreply.github.com>
2026-06-23 22:41:49 +0200

    release: bump version to 1.1.1 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.cuentamorosos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
